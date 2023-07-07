8cfd184c5d56c7ac7090baa9dd4d8275b509c53c
june3b <june3b@outlook.com>
2023-07-07 19:12:53 +0200

    final home

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,18 +1,10 @@
-
 if (window.innerWidth < 768) {
-	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
-		elem.classList.remove('animated');
-		elem.removeAttribute('data-bss-hover-animate');
-		elem.removeAttribute('data-aos');
-	});
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	AOS.init();
+$(document).ready(function(){
 
-	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
-	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
-		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
-		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
-	});
-}, false);
+	$('[data-bss-hover-animate]')
+		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
+		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
+});
